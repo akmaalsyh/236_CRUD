69634206b82199e126adda75cd40420c325da125
akmaalsyh <taufansyah50@gmail.com>
2025-10-21 10:02:08 +0700

    menambahkan fungsi get (untuk mengirim respon teks) dan listen (menampilkan pesan teks)

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
